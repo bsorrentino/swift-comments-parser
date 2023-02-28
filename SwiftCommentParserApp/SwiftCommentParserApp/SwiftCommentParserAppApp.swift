e744d5426877daa1e9e0951b2f10c29b3654720f
Build Pipeline <build.pipeline@softphone.it>
2023-02-28 17:45:35 +0100

    build: initial import

---
+++ SwiftCommentParserApp/SwiftCommentParserApp/SwiftCommentParserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCommentParserAppApp.swift
+//  SwiftCommentParserApp
+//
+//  Created by Bartolomeo Sorrentino on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCommentParserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
