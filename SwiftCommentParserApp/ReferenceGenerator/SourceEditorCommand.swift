0cfddb4d8e583b9924338c4cd323230d55e1e243
Build Pipeline <build.pipeline@softphone.it>
2023-02-28 18:06:23 +0100

    feat: add Xcode extension target

---
+++ SwiftCommentParserApp/ReferenceGenerator/SourceEditorCommand.swift
@@ -0,0 +1,19 @@
+//
+//  SourceEditorCommand.swift
+//  ReferenceGenerator
+//
+//  Created by Bartolomeo Sorrentino on 28/02/23.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
